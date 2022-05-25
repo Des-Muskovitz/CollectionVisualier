8d0477c8d54b0689b8c28e2a35f13f140b5418b6
Samuel Muskovitz <samuel.muskovitz@gmail.com>
2022-05-25 11:04:30 -0400

    Added test file to make sure image displaying works, imported image importing and image displaying libraries

--- src/main/java/com/CollectionVisualizer/Main.java
+++ src/main/java/com/CollectionVisualizer/Main.java
@@ -1,8 +1,27 @@
 package main.java.com.CollectionVisualizer;
 
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
 public class Main {
 
-    public static void main(String args[]){
-        System.out.println("Hello World!");
+    public static void main(String args[]) throws IOException {
+        String imagePath = "src\\main\\java\\com\\CollectionVisualizer\\2.png";
+        BufferedImage myPicture = ImageIO.read(new File(imagePath));
+
+        JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+
+        JPanel jPanel = new JPanel();
+        jPanel.add(picLabel);
+
+        JFrame f = new JFrame();
+        f.setSize(new Dimension(myPicture.getWidth(), myPicture.getHeight()));
+        f.add(jPanel);
+        f.setVisible(true);
     }
 }
